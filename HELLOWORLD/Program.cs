9d3cbd148d1a512fd8349ab514125f6ac0b69d4a
Steve Sutton <steven.p.sutton@gmail.com>
2020-10-12 16:36:12 +0100

    Initial creation in VS 2019

---
+++ HELLOWORLD/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+// Added for building in App Center
+//using Microsoft.AppCenter;
+//using Microsoft.AppCenter.Analytics;
+//using Microsoft.AppCenter.Crashes;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLOWORLD");
+            // Added for building in App Center
+            //AppCenter.Start("86db0035-1c9f-4ed8-bd0e-4c280a8b16dc",
+            //       typeof(Analytics), typeof(Crashes));
+
+        }
+    }
+}
