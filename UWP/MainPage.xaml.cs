10446b326e426aa29e5ca64b442cd169f23063a9
steven-p-sutton <steven.p.sutton@gmail.com>
2020-10-22 15:40:40 +0100

    added use of messageDialog box

--- UWP/MainPage.xaml.cs
+++ UWP/MainPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -34,6 +37,43 @@ namespace UWP
             Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello World");
             mediaElement.SetSource(stream, stream.ContentType);
             mediaElement.Play();
+            displayMessageAsync("Hello World", "'Hello World'", string.Empty);
+        }
+
+        //Stackoverflow Exampe of Dialog Box
+        public async Task displayMessageAsync(String title, String content, String dialogType)
+        {
+            var messageDialog = new MessageDialog(content, title);
+            if (dialogType == "notification")
+            {
+                //Do nothing here.Display normal notification MessageDialog
+            }
+            else
+            {
+                //Dipplay questions-Yes or No- MessageDialog
+                //messageDialog.Commands.Add(new UICommand("Yes", null));
+                //messageDialog.Commands.Add(new UICommand("No", null));
+                messageDialog.Commands.Add(new UICommand("OK", null));
+                messageDialog.DefaultCommandIndex = 0;
+            }
+
+            messageDialog.CancelCommandIndex = 1;
+            var cmdResult = await messageDialog.ShowAsync();
+
+            Debug.WriteLine("My Dialog answer label is:: " + cmdResult.Label);
+
+            if (cmdResult.Label == "Yes")
+            {
+                Debug.WriteLine("My Dialog answer label is:: " + cmdResult.Label);
+            }
+            else if (cmdResult.Label == "No")
+            {
+                Debug.WriteLine("My Dialog answer label is:: " + cmdResult.Label);
+            }
+            else
+            {
+                Debug.WriteLine("My Dialog answer label is:: " + cmdResult.Label);
+            }
         }
     }
 }
