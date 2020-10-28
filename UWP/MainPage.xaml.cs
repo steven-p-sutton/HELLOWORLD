82d46464d80927ade3dcf640fd71ee4d3211d8e3
steven-p-sutton <steven.p.sutton@gmail.com>
2020-10-28 21:36:52 +0000

    Replaced button clicked message dialog with a binging to CS page with string containing salutation to display

--- UWP/MainPage.xaml.cs
+++ UWP/MainPage.xaml.cs
@@ -30,50 +30,6 @@ namespace UWP
             this.InitializeComponent();
         }
 
-        public async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MediaElement mediaElement = new MediaElement();
-            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
-            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello World");
-            mediaElement.SetSource(stream, stream.ContentType);
-            mediaElement.Play();
-            await displayMessageAsync("Hello World", "'Hello World'", string.Empty);
-        }
-
-        //Stackoverflow Exampe of Dialog Box
-        public async Task displayMessageAsync(String title, String content, String dialogType)
-        {
-            var messageDialog = new MessageDialog(content, title);
-            if (dialogType == "notification")
-            {
-                //Do nothing here.Display normal notification MessageDialog
-            }
-            else
-            {
-                //Dipplay questions-Yes or No- MessageDialog
-                //messageDialog.Commands.Add(new UICommand("Yes", null));
-                //messageDialog.Commands.Add(new UICommand("No", null));
-                messageDialog.Commands.Add(new UICommand("OK", null));
-                messageDialog.DefaultCommandIndex = 0;
-            }
-
-            messageDialog.CancelCommandIndex = 1;
-            var cmdResult = await messageDialog.ShowAsync();
-
-            Debug.WriteLine("My Dialog answer label is:: " + cmdResult.Label);
-
-            if (cmdResult.Label == "Yes")
-            {
-                Debug.WriteLine("My Dialog answer label is:: " + cmdResult.Label);
-            }
-            else if (cmdResult.Label == "No")
-            {
-                Debug.WriteLine("My Dialog answer label is:: " + cmdResult.Label);
-            }
-            else
-            {
-                Debug.WriteLine("My Dialog answer label is:: " + cmdResult.Label);
-            }
-        }
+        public string _Salutation = "Hello World!";
     }
 }
