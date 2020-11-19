7c6af8c27884894b8b0f6dc698ff37c35774952b
steven-p-sutton <steven.p.sutton@gmail.com>
2020-11-19 11:35:46 +0000

    updates

--- HELLOWWORLD-EFCore/Program.cs
+++ HELLOWWORLD-EFCore/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Linq;
-
 // 
 // Migrations Overview - Migrstion using cmd line
 //   https://docs.microsoft.com/en-us/ef/core/managing-schemas/migrations/?tabs=dotnet-core-cli
 //
 //   1. Build the model in code
 //   2. Create the db using migration
+//      - dotnet ef migrations add InitialCreate
+//      - dotnet ef database update
 //   3. Revise the model in code 
 //   4. Upgrade the db using migration
+//      - dotnet ef migrations add <abc>
+//      - dotnet ef database update
 //   5. Repeat code -> upgrade steps as required
-//   6. Copy db from soiurec folder to deplyment folder before running, else exceptions will occur 
+//   6. Copy db from source folder to deployment folder before running, else exceptions will occur 
 //      (cant fins db so new blank created and tables are seen as missing, exception rised by app)
 // SQLite Viewer Download - use to view the blogging.db created by the migration
 //   https://www.systoolsgroup.com/sqlite-viewer.html
